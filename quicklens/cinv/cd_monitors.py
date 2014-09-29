5f1dfa14f32905deef9fd3e8afa4d02f79a7f460
Duncan Hanson <duncan.hanson@gmail.com>
2014-09-29 16:19:29 -0400

    update documentation for cinv/cd_monitors.py

--- quicklens/cinv/cd_monitors.py
+++ quicklens/cinv/cd_monitors.py
@@ -1,16 +1,19 @@
 # quicklens/cinv/cd_monitors.py
 # --
-# 
+# classes and routines for monitoring/logging the conjugate directions solver in cd_solve.py.
+#  monitors = object which is called at each descent iteration, responsible for deciding when converged and also for coordinating any logging/status updates.
+#  logger   = function which are called on by a descent monitor to print or log status updates.
 
 import sys
 import numpy as np
 
 from .. import util
 
-## monitors
-logger_basic = (lambda iter, eps, watch=None, **kwargs : sys.stdout.write( '[' + str(watch.elapsed()) + '] ' + str((iter, eps)) + '\n' ))
-logger_none  = (lambda iter, eps, watch=None, **kwargs : 0)
+logger_basic = (lambda it, eps, watch=None, **kwargs : sys.stdout.write( '[' + str(watch.elapsed()) + '] ' + str((it, eps)) + '\n' ))
+logger_none  = (lambda it, eps, watch=None, **kwargs : 0)
+
 class monitor_basic():
+    """ a simple class for monitoring a conjugate descent iteration. """
     def __init__(self, dot_op, iter_max=np.inf, eps_min=1.0e-10, logger=logger_basic):
         self.dot_op   = dot_op
         self.iter_max = iter_max
@@ -19,16 +22,16 @@ class monitor_basic():
 
         self.watch = util.stopwatch()
 
-    def criterion(self, iter, soltn, resid):
+    def criterion(self, it, soltn, resid):
         delta = self.dot_op( resid, resid )
         
-        if (iter == 0):
+        if (it == 0):
             self.d0 = delta
 
-        if (self.logger is not None): self.logger( iter, np.sqrt(delta/self.d0), watch=self.watch,
+        if (self.logger is not None): self.logger( it, np.sqrt(delta/self.d0), watch=self.watch,
                                                    soltn=soltn, resid=resid )
 
-        if (iter >= self.iter_max) or (delta <= self.eps_min**2 * self.d0):
+        if (it >= self.iter_max) or (delta <= self.eps_min**2 * self.d0):
             return True
 
         return False
