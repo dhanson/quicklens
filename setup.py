006c53240eac1b5bddd84166d9158c590e07bb89
Duncan Hanson <duncan.hanson@gmail.com>
2014-10-27 13:23:18 -0400

    add a setup.py script for installing as well as building the SHT Fortran code.

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# to compile fortran code for spherical harmonic transforms inplace, run
+# python setup.py build_ext --inplace --fcompiler=gnu95
+
+import glob
+import numpy as np
+import distutils
+
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('quicklens',parent_package,top_path)
+
+    if distutils.version.StrictVersion(np.version.version) > distutils.version.StrictVersion('1.6.1'):
+        config.add_extension('quicklens.shts.fsht', ['quicklens/shts/shts.f95'],
+                             libraries=['gomp'], f2py_options=[],
+                             extra_f90_compile_args=['-ffixed-line-length-1000', '-fopenmp'],
+                             extra_compile_args=['-fopenmp'], extra_link_args=[],)
+    else:
+        config.add_extension('quicklens.shts.fsht', ['quicklens/shts/shts.f95'],
+                             libraries=['gomp'], f2py_options=[],
+                             extra_compile_args=['-fopenmp'], extra_link_args=[],)
+
+    return config
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+
+    setup(name='lensing',
+          configuration=configuration)
