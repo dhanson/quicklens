f61bf73fb9ba70a5c74d7be717df82309c0fb1e8
apetri <apetri@phys.columbia.edu>
2017-02-20 09:24:13 -0500

    fix setup.py for distribution

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('quicklens',parent_package,top_path)
 
-    config.add_extension('quicklens.math.cwignerd', ['quicklens/math/wignerd.pyf', 'quicklens/math/wignerd.c'])
+    config.add_extension('math.cwignerd', ['quicklens/math/wignerd.pyf', 'quicklens/math/wignerd.c'])
     if distutils.version.StrictVersion(np.version.version) > distutils.version.StrictVersion('1.6.1'):
-        config.add_extension('quicklens.shts.fsht', ['quicklens/shts/shts.f95'],
+        config.add_extension('shts.fsht', ['quicklens/shts/shts.f95'],
                              libraries=['gomp'], f2py_options=[],
                              extra_f90_compile_args=['-ffixed-line-length-1000', '-fopenmp'],
                              extra_compile_args=['-fopenmp'], extra_link_args=[],)
     else:
-        config.add_extension('quicklens.shts.fsht', ['quicklens/shts/shts.f95'],
+        config.add_extension('shts.fsht', ['quicklens/shts/shts.f95'],
                              libraries=['gomp'], f2py_options=[],
                              extra_compile_args=['-fopenmp'], extra_link_args=[],)
 
@@ -26,5 +26,5 @@ def configuration(parent_package='',top_path=None):
 if __name__ == "__main__":
     from numpy.distutils.core import setup
 
-    setup(name='lensing',
+    setup(packages=["quicklens","quicklens.cinv","quicklens.qest","quicklens.sims","quicklens.shts","quicklens.math"],
           configuration=configuration)
